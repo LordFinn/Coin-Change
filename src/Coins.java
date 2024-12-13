b495f9411535c3b5c264d7fa26a956a7c8162796
LordFinn <176671863+LordFinn@users.noreply.github.com>
2024-12-13 17:08:27 +0100

    Do some infrastructure

---
+++ src/Coins.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Coins {
+
+    private static List<Integer> calculateMinimalCoins(int amount, List<Integer> coinValues) {
+        List<Integer> returnList = new ArrayList<>();
+        return returnList;
+    }
+
+    public static void main(String[] args) {
+        // TODO: create some examples here
+    }
+
+}
