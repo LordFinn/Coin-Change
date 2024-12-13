0a18cbd6fa80d6c199389984131ececb98dd003a
LordFinn <176671863+LordFinn@users.noreply.github.com>
2024-12-13 17:29:00 +0100

    Add algorithm implementation Only one solution yet tho

--- src/Coins.java
+++ src/Coins.java
@@ -3,8 +3,29 @@ import java.util.List;
 
 public class Coins {
 
-    private static List<Integer> calculateMinimalCoins(int amount, List<Integer> coinValues) {
+    private static List<Integer> calculateMinimalCoins(int n, List<Integer> coins) {
+
+        int[] numberCoins = new int[n + 1];
+        int[] nextCoins  = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            int bestNumber = Integer.MAX_VALUE;
+            int bestCoin = 0;
+            for (int coin : coins) {
+                if ((i >= coin) && (numberCoins[i - coin] + 1 < bestNumber)) {
+                    bestNumber = numberCoins[i - coin] + 1;
+                    bestCoin = coin;
+                }
+            }
+            numberCoins[i] = bestNumber;
+            nextCoins[i] = bestCoin;
+        }
+
         List<Integer> returnList = new ArrayList<>();
+        while (n != 0) {
+            returnList.add(nextCoins[n]);
+            n = n - nextCoins[n];
+        }
         return returnList;
     }
 
